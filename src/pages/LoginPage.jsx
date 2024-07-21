1f521f1923930d5607ee0c9557241c6de16adde3
prabowo-sakti <prabowosakti2023@gmail.com>
2024-07-21 12:05:43 +0700

    menambahkan logic redirect dan menghapus .html

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -19,32 +19,55 @@ function Login() {
         </div>
         <h4 className="text-4xl font-semibold my-5">Login</h4>
         <form className="flex flex-col w-3/4">
-          <input type="text" placeholder="Enter Email" className="w-full py-4 px-6 text-lg placeholder:text-slate-900  rounded mb-4 bg-[#FEEED9]" />
-          <input type="password" placeholder="Enter Password" className="w-full py-4 px-6 text-lg placeholder:text-slate-900  rounded mb-4 bg-[#FEEED9]" />
-          <button className="w-full text-slate-900 rounded-lg block text-center py-2 px-4 mx-auto bg-primary text-white font-bold text-3xl tracking-widest">LOGIN</button>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            className="w-full py-4 px-6 text-lg placeholder:text-slate-900  rounded mb-4 bg-[#FEEED9]"
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            className="w-full py-4 px-6 text-lg placeholder:text-slate-900  rounded mb-4 bg-[#FEEED9]"
+          />
+          <button className="w-full text-slate-900 rounded-lg block text-center py-2 px-4 mx-auto bg-primary text-white font-bold text-3xl tracking-widest">
+            LOGIN
+          </button>
         </form>
         <div className="flex gap-2 items-center my-2 w-1/2">
           <hr className="w-1/2 bg-black" />
           <p className="text-2xl font-light">OR</p>
           <hr className="w-1/2 bg-black" />
         </div>
-        <a href="./register.html" className="text-slate-900 rounded block text-center py-1 px-4 mx-auto border-4 border-[#87C4FF] w-3/4 font-semibold text-3xl tracking-widest">
+        <a
+          href="/register"
+          className="text-slate-900 rounded block text-center py-1 px-4 mx-auto border-4 border-[#87C4FF] w-3/4 font-semibold text-3xl tracking-widest"
+        >
           FACEBOOK
         </a>
         <div className="flex flex-row gap-3 text-sm justify-center my-2">
-          <a href="./login.html" className="hover:text-primary-dark text-slate-900 mt-5 inline-block">
+          <a
+            href="/login"
+            className="hover:text-primary-dark text-slate-900 mt-5 inline-block"
+          >
             Forget Password? |
           </a>
 
           <p>
             Need New Account?{" "}
-            <a href="./register.html" className="hover:text-primary-dark text-slate-900 mt-5 inline-block font-bold">
+            <a
+              href="/register"
+              className="hover:text-primary-dark text-slate-900 mt-5 inline-block font-bold"
+            >
               Sign Up
             </a>
           </p>
         </div>
         {/* <!-- blue waves start  --> */}
-        <img src="./../src/assets/icons/waves-top.svg" alt="wave top" className="w-full mt-5" />
+        <img
+          src="./../src/assets/icons/waves-top.svg"
+          alt="wave top"
+          className="w-full mt-5"
+        />
       </div>
     </>
   );
